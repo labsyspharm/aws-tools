24f31821f786f0c32e31ddb700583e285b0bd637
Jeremy Muhlich <jmuhlich@bitflood.org>
2020-09-15 13:21:03 -0400

    Switch to unblended cost

--- costs/costs_by_project.sh
+++ costs/costs_by_project.sh
@@ -45,7 +45,7 @@ jq \
     .ResultsByTime[0].Groups
     | map([
         (.Keys[0] | sub("^project\\$"; "")),
-        (.Metrics.BlendedCost.Amount | tonumber)
+        (.Metrics.UnblendedCost.Amount | tonumber)
     ])
     | .[]
     | @csv
--- costs/costs_by_service_no_project.sh
+++ costs/costs_by_service_no_project.sh
@@ -49,7 +49,7 @@ jq \
     .ResultsByTime[0].Groups
     | map([
         (.Keys[0]),
-        (.Metrics.BlendedCost.Amount | tonumber)
+        (.Metrics.UnblendedCost.Amount | tonumber)
     ])
     | .[]
     | @csv
